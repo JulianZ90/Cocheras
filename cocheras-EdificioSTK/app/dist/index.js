aba19218f4992731f0f290b2da07eecc1be711cb
Julian Zelayeta <julian.zelayeta@softtek.com>
2017-02-20 16:54:52 -0300

    asdasd

--- cocheras-EdificioSTK/app/dist/index.js
+++ cocheras-EdificioSTK/app/dist/index.js
@@ -1,31 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 class Button extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.get = this.get.bind(this);
+    this.mostrarRespuesta = this.mostrarRespuesta.bind(this);
+  }
+
+  vaciar(){
+    var formulario = document.getElementById("app");
+    formulario.innerHTML = '';
+  }
+
+  get(){
+    fetch('http://172.16.82.136:4567/hello')
+    // .then( response  => {
+    //   return response.json()
+    // })
+  }
+
+  mostrarRespuesta(){
+      var formulario = document.getElementById("app");
+      formulario.innerHTML = this.get();
+  }
   render() {
     return (
-      <button className="boton">
-        OK
-        {/* TODO */}
-      </button>
+      <input type="button" value="OK" id="btn" class ="boton" onClick = {this.mostrarRespuesta}/>
     );
   }
 }
 
-class Form extends React.Component{
-  renderButton() {
-    return <Button />;
-  }
+class Input extends React.Component {
   render(){
     return(
-    <form>
-      Ingrese nombre : <input type="text" name="nombre"></input>
-      {this.renderButton()}
-    </form>
-
-  );
+      <form>
+        Ingrese nombre : <input type="text" name="nombre"></input>
+      </form>
+    )
   }
 }
+
+class Form extends React.Component{
+  render(){
+    return(
+      <form id="app" ref="form" >
+        <Input/><Button/>
+      </form>
+    );
+  }
+}
+
 
 // ========================================
 
